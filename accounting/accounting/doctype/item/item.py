6000fe97a8e27769ab45039c462c698322593236
phot0n <ritwikpuri5678@gmail.com>
2021-11-03 21:25:45 +0530

    fix: item page(route) should not be accessible when show_in_website is not checked

--- accounting/accounting/doctype/item/item.py
+++ accounting/accounting/doctype/item/item.py
@@ -1,8 +1,13 @@
 # Copyright (c) 2021, ritwik and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.website.website_generator import WebsiteGenerator
 
 class Item(WebsiteGenerator):
-	pass
+	def validate(self):
+		if not self.show_in_website:
+			self.route = None
+		else:
+			item_base_route = frappe.get_doc("DocType", "Item").route
+			self.route = item_base_route + f"/{self.name}"
